14c29962892bb3097c7d2a50046958f0abee043f
ch2isk4kos <ch2isk4kos@gmail.com>
2019-11-08 22:36:13 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+let divide = function() {
+    let dividend = 2000 / 100;
+    return dividend;
+}
+
+// let divide = function(x, y) {
+//     let sum = x / y;
+//     return sum;
+// }
+
+let square = (e => e * e);
+
+let add = ((x, y) => x + y);
+
+// let add = (x, y) => {
+//     return x + y;
+// }
