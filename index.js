1eaea2dcbb76f51a77e3cf63bda18011e9006f39
Christopher Dent <christopherdent01@gmail.com>
2020-06-03 20:27:24 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,13 @@
+// function divide(parameter1, parameter2) {
+//   let quotient = parameter1 / parameter2
+//   return quotient
+// }
+
+
+let divide = function() {
+  return 2000/100
+};
+
+
+let square = (number) => number * number
+let add = (parameter1, parameter2) => parameter1 + parameter2
