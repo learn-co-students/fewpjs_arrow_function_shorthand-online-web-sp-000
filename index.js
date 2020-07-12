364b3b0f3c2726cf0f407b01e77da4e4968d896f
Andrew-J-Williams <1williamsaj@gmail.com>
2020-07-12 12:08:52 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,9 @@
+const divide = function(){
+    return 2000/100 // The anonymous function is to the right of the "="
+}
+
+const square = x => x ** x // Since it only has one parameter, we do not need '()' with an arrow function
+
+const add = (x, y) => x + y // With multiple paramters, we then must use '()'.
+
+
