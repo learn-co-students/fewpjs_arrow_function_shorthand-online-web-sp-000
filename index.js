63cce7f63eabd8dabb0c5c85456ac7e18ad1d58e
github username <github email address>
2020-08-05 09:56:26 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+let divide = function(num1 = 2000, num2 = 100){
+return (num1 / num2);
+}
+
+divide();
+
+
+let square = num => {
+    return (num * num);
+}
+
+square();
+
+
+let add = (num1, num2) => {
+    return (num1 + num2);
+}
+
+add(1, 2);
